5f6fe9cbe6843b33a8fb79f22b426505d71d73db
Cameron Corda <ccorda@gmail.com>
2013-04-05 09:28:46 -0700

    adding git aliases and using hub as a git wrapper

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,7 +5,24 @@ alias ....="cd ../../.."
 alias .....="cd ../../../.."
 
 # git
+
+# Use `hub` as our git wrapper:
+#   http://defunkt.github.com/hub/
+hub_path=$(which hub)
+if (( $+commands[hub] ))
+then
+  alias git=$hub_path
+fi
+
+alias gp='git push'
 alias gl='git pull'
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gd='git diff'
+alias gc='git commit'
+alias gca='git commit -a'
+alias gco='git checkout'
+alias gb='git branch'
+alias gs='git status -sb'
 
 # python
 alias pys='python -m SimpleHTTPServer 8888'
