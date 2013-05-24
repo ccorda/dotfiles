808555cef9b0c3a2b8782b714b84771333a8453f
Cameron Corda <ccorda@gmail.com>
2013-05-24 05:09:56 +0100

    aliases for common grunt commands

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,6 +4,7 @@ alias ...="cd ../.."
 alias ....="cd ../../.."
 alias .....="cd ../../../.."
 
+# get to project folder quicker
 function p {
   project=$1
   cd ~/projects/${1}
@@ -19,6 +20,7 @@ then
   alias git=$hub_path
 fi
 
+# git
 alias gp='git push'
 alias gl='git pull'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
@@ -28,6 +30,10 @@ alias gca='git commit -a'
 alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status -sb'
+
+# grunt
+alias g='grunt'
+alias gr='grunt release'
 
 # python
 alias pys='python -m SimpleHTTPServer 8888'
