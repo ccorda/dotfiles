ba1d84577e3b8ac7899af7beea5f13c944332d61
Cameron Corda <ccorda@gmail.com>
2013-07-31 17:02:27 -0700

    SQL helpers

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -35,12 +35,32 @@ alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status -sb'
 
-# grunt
+#### grunt ####
 alias g='grunt'
 alias gr='grunt release'
 
-# python
+#### python ####
 alias pys='python -m SimpleHTTPServer 8888'
 
-# apache
-alias apr='sudo apachectl restart'
+#### sql ####
+alias sqlaf='node ~/projects/jts-sandbox/appfog-database-export/export.js'
+
+function sqld {
+    # usage: sqld project
+    # todo: based on the filename, just grab the DB automatically
+    mysqldump -u root -h 127.0.0.1 $1 --add-drop-table | gzip > $1-$(date +%Y-%m-%d-%H%M).sql.gz
+}
+
+function sqli {
+    # to load a specific file (todo)
+    # or to grab the most recent .gz file
+    # usage: sqli project
+    sql=`ls -Art *.gz | tail -n 1`
+    echo $sql
+    gunzip < $sql | mysql -u root -h 127.0.0.1 $1
+    echo 'import complete'
+    # todo: based on the filename, just grab the DB automatically
+}
+
+# zsh
+alias zr='. ~/.zshrc' #refresh shell
