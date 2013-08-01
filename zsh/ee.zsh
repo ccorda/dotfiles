327f63408b3470b0f84bb4742bfefa4a01ee44ff
Cameron Corda <ccorda@gmail.com>
2013-07-31 17:04:01 -0700

    Adding new sublime prefs

--- zsh/ee.zsh
+++ zsh/ee.zsh
@@ -3,10 +3,5 @@
 # $1 = project
 # $2 = db name (optional, otherwise will default to project value)
 function eedump {
-  if [ -z "$2" ]
-  then
-    mysqldump -u root -h localhost $2 --add-drop-table | gzip > $1-$(date +%Y-%m-%d-%H%M).sql.gz
-  else
-    mysqldump -u root -h localhost $1 --add-drop-table | gzip > $1-$(date +%Y-%m-%d-%H%M).sql.gz
-  fi
+  mysqldump -u root -h localhost $1 --add-drop-table | gzip > $1-$(date +%Y-%m-%d-%H%M).sql.gz
 }
